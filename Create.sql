f1a614d51fda70021d41b2497bd52a2340bcc694
B Leston Bandeira <barbara.bandeira.t0114615@edu.atec.pt>
2019-09-03 22:18:37 +0000

    Upload New File - Create SQL

---
+++ Create.sql
@@ -0,0 +1,13 @@
+use TPSIP1118_recrutamento
+go
+
+CREATE TABLE Sexo (
+	id_sexo integer Primary Key,
+	valor_sexo varchar(50)
+	)
+GO
+
+INSERTO INTO Sexo VALUES
+	(1, 'feminino'),
+	(2, 'masculino)
+
