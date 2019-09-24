fcbf383dae9cefb576095895a314b3898c3729dc
sergiomoura <sergio.moura.t0114628@edu.atec.pt>
2019-09-24 23:09:04 +0100

    CRUD & MIGRATIONS (1)

---
+++ projeto/app/Http/Controllers/LiteracyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Literacy;
+use Illuminate\Http\Request;
+
+class LiteracyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Literacy  $literacy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Literacy $literacy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Literacy  $literacy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Literacy $literacy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Literacy  $literacy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Literacy $literacy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Literacy  $literacy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Literacy $literacy)
+    {
+        //
+    }
+}
---
+++ projeto/app/Http/Controllers/UnemploymentSituationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UnemploymentSituation;
+use Illuminate\Http\Request;
+
+class UnemploymentSituationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UnemploymentSituation  $unemploymentSituation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UnemploymentSituation $unemploymentSituation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UnemploymentSituation  $unemploymentSituation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UnemploymentSituation $unemploymentSituation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UnemploymentSituation  $unemploymentSituation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UnemploymentSituation $unemploymentSituation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UnemploymentSituation  $unemploymentSituation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UnemploymentSituation $unemploymentSituation)
+    {
+        //
+    }
+}
