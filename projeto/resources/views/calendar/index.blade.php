4d689a8d484becab3fa03152443f29f72b61a79c
sergiomoura <sergio.moura.t0114628@edu.atec.pt>
2019-10-08 20:04:41 +0100

    CRUD CALENDAR

--- projeto/resources/views/calendar/index.blade.php
+++ projeto/resources/views/calendar/index.blade.php
@@ -1,96 +1,117 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col text-left">
-                        <h2></h2>
-                    </div>
-                    <div class="col text-right">
-                        <a href="/applicants/create"><button type="button" class="btn btn-primary">NEW</button></a>
-                    </div>
-                </div>
-
-
-                    
-                <div class="container-fluid">
-                    <div class="row">
-                    @foreach ($applicants as $applicant)    
-                        <div class="col-xl-2" style="margin: 5px; margin-bottom: 30px; padding: 10px; border-style: solid;border-color: rgb(0, 162, 255); border-radius: 7px; min-width: 14vw; word-wrap: break-word;">
-                            <h2>
-                                <a href="{{ url('/applicants/' . $applicant->id ) }}">{{$applicant->name}}</a>
-                            </h2>
-                            <p>
-                                {{ Str::limit($applicant->email, 30)}}
-                            </p>
-                            <p>
-                                <a class="btn btn-primary btn-sm" href="{{ url('/applicants/' . $applicant->id ) }}" role="button">Detalhes</a>
-                            </p>
-                        </div>
-                    @endforeach
-                    </div>
-                </div>
-                
+<?php
+//index.php
 
 
 
-</hr>
 
-                <div class="row">
-                    <div class="col-md-12">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        ID
-                                    </th>
-                                    <th>
-                                        NAME
-                                    </th>
-                                    <th>
-                                        EMAIL
-                                    </th>
-                                    <th>
-                                        TOWN
-                                    </th>
-                                    <th>
-                                        do
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($applicants as $applicant)
-                                <tr>
-                                    <td>
-                                        {{$applicant->id}}
-                                    </td>
-                                    <td>
-                                        <a href="{{ url('/applicants/' . $applicant->id ) }}">{{$applicant->name}}</a>
-                                    </td>
-                                    <td>
-                                        {{ Str::limit($applicant->email, 30)}}
-                                    </td>
-                                    <td>
-                                        {{$applicant->town}}
-                                    </td>
-                                    <td>
-                                        <form action="/applicants/{{ $applicant->id }}" method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <input type="submit" class="btn btn-danger btn-sm" value="DELETE">
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Jquery Fullcalandar Integration with PHP and Mysql</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+  <script>
+   
+  $(document).ready(function() {
+   var calendar = $('#calendar').fullCalendar({
+    editable:true,
+    header:{
+     left:'prev,next today',
+     center:'title',
+     right:'month,agendaWeek,agendaDay'
+    },
+    events: 'load.php',
+    selectable:true,
+    selectHelper:true,
+    select: function(start, end, allDay)
+    {
+     var title = prompt("Enter Event Title");
+     if(title)
+     {
+      var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+      var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+      $.ajax({
+       url:"insert.php",
+       type:"POST",
+       data:{title:title, start:start, end:end},
+       success:function()
+       {
+        calendar.fullCalendar('refetchEvents');
+        alert("Added Successfully");
+       }
+      })
+     }
+    },
+    editable:true,
+    eventResize:function(event)
+    {
+     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+     var title = event.title;
+     var id = event.id;
+     $.ajax({
+      url:"update.php",
+      type:"POST",
+      data:{title:title, start:start, end:end, id:id},
+      success:function(){
+       calendar.fullCalendar('refetchEvents');
+       alert('Event Update');
+      }
+     })
+    },
+
+    eventDrop:function(event)
+    {
+     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+     var title = event.title;
+     var id = event.id;
+     $.ajax({
+      url:"update.php",
+      type:"POST",
+      data:{title:title, start:start, end:end, id:id},
+      success:function()
+      {
+       calendar.fullCalendar('refetchEvents');
+       alert("Event Updated");
+      }
+     });
+    },
+
+    eventClick:function(event)
+    {
+     if(confirm("Are you sure you want to remove it?"))
+     {
+      var id = event.id;
+      $.ajax({
+       url:"delete.php",
+       type:"POST",
+       data:{id:id},
+       success:function()
+       {
+        calendar.fullCalendar('refetchEvents');
+        alert("Event Removed");
+       }
+      })
+     }
+    },
+
+   });
+  });
+   
+  </script>
+ </head>
+ <body>
+  <br />
+  <h2 align="center"><a href="#">Jquery Fullcalandar Integration with PHP and Mysql</a></h2>
+  <br />
+  <div class="container">
+   <div id="calendar"></div>
+  </div>
+ </body>
+</html>
