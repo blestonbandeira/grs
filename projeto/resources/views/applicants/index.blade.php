9574cc34228b139625477e83e074586d46e461b5
Ines Fialho <inesespinhaa@gmail.com>
2019-10-23 23:21:59 +0100

    layout candidatos

--- projeto/resources/views/applicants/index.blade.php
+++ projeto/resources/views/applicants/index.blade.php
@@ -1,7 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<div class="col text-left">
+    <a href="/applicants/create"><button type="button" class="btn btn-info">Adicionar</button></a>
+</div>
+<div class="card">
+    <div class="card-header card-header-info">
+      <h4 class="card-title">Turma TPSI11.18</h4>
+      <p class="card-category">Início: novembro 2018</p>
+    </div>
+    <div class="card-body table-responsive">
+      <table class="table table-hover">
+        <thead class="text-info">
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Cidade</th>
+          <th>Gerir</th>
+        </thead>
+        <tbody>
+        @foreach ($applicants as $applicant)
+            <tr>
+                <td>
+                    {{$applicant->id}}
+                </td>
+                <td>
+                    {{$applicant->name}}
+                </td>
+                <td>
+                    {{ Str::limit($applicant->email, 30)}}
+                </td>
+                <td>
+                    {{$applicant->town}}
+                </td>
+                <td class="d-flex">
+                    {{-- <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
+                        <i class="material-icons">edit</i>
+                    </button> --}}
+
+                    <form action="/applicants/{{ $applicant->id }}" method="post">
+                        @csrf
+                        @method('delete')
+
+
+                        {{-- <button type="submit" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm"  value="DELETE">
+                            <i class="material-icons">close</i>
+                        </button> --}}
+
+
+                        <input type="submit" class="btn btn-danger btn-sm" value="DELETE">
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="container-fluid">
@@ -69,7 +150,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 <script>
     // Add active class to the current button (highlight it)
     try{
@@ -82,6 +163,6 @@
         document.getElementById("btnApplicant").className += " active";
     }
     document.getElementById("btnApplicant").className += " active";
-    
+
 </script>
 @endsection
