03765012d548078638b36dcd44d06022be3cefcb
sergiomoura <sergio.moura.t0114628@edu.atec.pt>
2019-10-27 11:20:31 +0000

    For ApiINTERVIEW

--- projeto/app/Http/Controllers/ApiInterviewController.php
+++ projeto/app/Http/Controllers/ApiInterviewController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use App\ApplicantEvent;
 use Illuminate\Http\Request;
 use App\Interview;
-use App\InterviewInterviewer;
+use App\InterviewInterviewers;
 use Carbon\Carbon;
+use App\Event;
 use Auth;
 
 class ApiInterviewController extends Controller
@@ -29,28 +30,33 @@ class ApiInterviewController extends Controller
      */
     public function store(Request $request)
     {
+        for($i = 0; $i < $request->count; $i++)
+            dd($request[$i]);
+
+
+
         $interview = new Interview;
         $interview->id_applicant = $request->id_applicants;
         $time = Carbon::parse($request->date);
         $interview->date = $time;
         $interview->save();
 
-        $interview_interviewer = new InterviewInterviewer();
+        $interview_interviewer = new InterviewInterviewers;
         $interview_interviewer->id_interview = $request->id_interview;
-        $interview_interviewer->id_user = Auth::id();
+        $interview_interviewer->id_user = 1;//Auth::id();
         $interview_interviewer->save();
 
         $event = new Event;
-        $event->id_user = Auth::id();
+        $event->id_user = 1;//Auth::id();
         $event->title = $request->title;
         $event->type = $request->type;
         $event->start_event = $request->start_event;
         $event->end_event = $request->end_event;
         $event->save();
 
-        $eventCreated = Event::Find($event);
+        $eventCreated = Event::where('title', '=', $event->title, 'and', 'start_event', '=', $event->start_event)->first();
         $appli_event = new ApplicantEvent;
-        $appli_event->id_applicant = $request->id_applicant;
+        $appli_event->id_applicant = $request->id_applicants;
         $appli_event->id_event = $eventCreated->id;
         $appli_event->save();
 
