6077915404eecf6224c5098fd1eb2d5b513e7677
Ines Fialho <inesespinhaa@gmail.com>
2019-11-07 18:44:45 +0000

    provas

--- projeto/resources/views/applicants/index.blade.php
+++ projeto/resources/views/applicants/index.blade.php
@@ -13,9 +13,46 @@
         </button>
     </a>
     <a href="#">
-        <button type="button" class="btn btn-info">
+        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">
             Marcar Prova
         </button>
+
+
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Marcar Prova</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                    <button type="button" class="btn btn-info" data-dismiss="modal">Teste & Prova</button>
+                    <button type="button" class="btn btn-info">Teste & Inventário</button>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </a>
 </div>
 
