6e4465a5352e755cb8f1c4be55e28c963f52b82c
blestonbandeira <blestonbandeira@gmail.com>
2019-11-07 01:56:10 +0000

    seeder: rsclass names generated automatically from id_courseName

--- projeto/database/seeds/RsClassesSeeder.php
+++ projeto/database/seeds/RsClassesSeeder.php
@@ -18,14 +18,34 @@ class RsClassesSeeder extends Seeder
     public function run()
     {
         $rsClass = new RsClass;
-        $rsClass->startDate =  "2019-12-03";
-        $users = User::select('id')->where('name', '=', 'Assist')->get();
+        $rsClass->startDate = "2019-12-03";
+        $users = User::get('id')->pluck('id');
         $rsClass->id_user = $users[0]["id"];
-        $classStates = ClassState::select('id')->where('name', '=', 'Activo');
-        $rsClass->id_classState = $classStates;
+        $classStates = ClassState::get('id')->pluck('id');
+        $rsClass->id_classState = $classStates[0]["id"];
         $courseNames = CourseName::select('name')->where('id', '=', '1')->get();
-        $rsClass->name = getCourseName($courseNames, "2019-12-03"); 
-        $rsClass->save();       
+        $rsClass->name = getCourseName($courseNames, $rsClass->startDate); 
+        $rsClass->save();      
+        
+        $rsClass = new RsClass;
+        $rsClass->startDate = "2019-12-03";
+        $users = User::get('id')->pluck('id');
+        $rsClass->id_user = $users[0]["id"];
+        $classStates = ClassState::get('id')->pluck('id');
+        $rsClass->id_classState = $classStates[0]["id"];
+        $courseNames = CourseName::select('name')->where('id', '=', '2')->get();
+        $rsClass->name = getCourseName($courseNames, $rsClass->startDate); 
+        $rsClass->save();    
+
+        $rsClass = new RsClass;
+        $rsClass->startDate = "2019-12-03";
+        $users = User::get('id')->pluck('id');
+        $rsClass->id_user = $users[0]["id"];
+        $classStates = ClassState::get('id')->pluck('id');
+        $rsClass->id_classState = $classStates[0]["id"];
+        $courseNames = CourseName::select('name')->where('id', '=', '7')->get();
+        $rsClass->name = getCourseName($courseNames, $rsClass->startDate); 
+        $rsClass->save();   
     
     }
 }
