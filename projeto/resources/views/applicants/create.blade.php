ed40eb3f920a2a157a796b86e454a272fa28eb56
inesespinha <inesespinhaa@gmail.com>
2019-11-15 22:26:04 +0000

    cenas

--- projeto/resources/views/applicants/create.blade.php
+++ projeto/resources/views/applicants/create.blade.php
@@ -12,59 +12,50 @@
             <div class="card-header card-header-text card-header-info">
               <div class="card-text">
                 <h4 class="card-title">Dados da Candidatura Online</h4>
-              </div>              
+              </div>
             </div>
-
             <div class="card-body">
-<<<<<<< HEAD
-
               <div class="row">
-                <div class="col-md-6">
-                  <div>
-                    <label class="label-create-form">Nome Completo</label>
-=======
-              <div class="row">                
-                <div class="col-md-8">
+                <div class="col-md-5">
+                  <div class="form-group">
+                    <label class="bmd-label-floating">Turma de Recrutamento</label>
+                    <datalist id="turma">
+                      <option value="TPSI11.18">
+                      <option value="TPSI10.18">
+                      <option value="GR09.17">
+                      <option value="GR09.18">
+                      <option value="GR09.19">
+                    </datalist>
+                    <input list="turma" class="form-control input-border-width">
+                  </div>
+                </div>
+                <div class="col-md-3">
                   <div class="form-group">
                     <label class="bmd-label-floating">Nome Completo</label>
->>>>>>> 0909441800312dd8d7123fd56baab60e6394d373
                     <input type="text" class="form-control input-border-width" name="name">
                   </div>
                 </div>
-                <div class="col-md-3">
-                  <div>
-                    <label class="label-create-form" name="email">Contacto</label>
-                    <input class="form-control input-border-width" type="text">
-                  </div>
-                </div>
-                <div class="col-md-3">
-                  <div>
-                    <label class="label-create-form">Email</label>
-                    <input type="email" class="form-control input-border-width" name="email">
-                  </div>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-4">
                   <div class="form-group">
                     <label class="bmd-label-floating bg-white p-1">Data-de-Nascimento</label>
                     <input type="date" class="form-control input-border-width" name="birthdate" value="birthdate" format="dd/MM/yyyy">
                   </div>
                 </div>
-                <div class="col-md-3">
+              </div>
+              <div class="row">
+                <div class="col-md-4">
                   <div class="form-group">
                     <label class="bmd-label-floating">N. Contribuinte</label>
                     <input type="text" class="form-control input-border-width" name="nif">
                   </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                   <div class="form-group">
                     <label class="bmd-label-floating">N. CC</label>
                     <input type="text" class="form-control input-border-width" name="identityCard">
                   </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                   <div class="form-group">
                     <label class="bmd-label-floating bg-white p-1">Data de Validade</label>
                     <input type="date" class="form-control input-border-width" name="ccExpirationDate">
@@ -112,39 +103,30 @@
                 </div>
               </div>
               <div class="row">
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <label class="bmd-label-floating" name="town">Localidade</label>
-                    <input type="email" class="form-control input-border-width">
-                  </div>
-                </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                   <div class="form-group">
                     <label class="bmd-label-floating">Concelho</label>
                     <input type="text" class="form-control input-border-width">
                   </div>
                 </div>
-                {{-- <div class="col-md-3">
+                <div class="col-md-3">
                   <div class="form-group">
                     <label class="bmd-label-floating">Distrito</label>
                     <input type="text" class="form-control input-border-width">
                   </div>
-                </div> --}}
-
-                <div class="col-md-4">
-                  <div>
-                    <label class="label-create-form pb-2">Distrito</label>
-                    <select name="id_district" class="custom-select input-border-width">
-                        @foreach($districts as $district)
-                              <option value="{{ $district->id }}">
-                                  {{ $district->name }}
-                              </option>
-                          @endforeach
-                    </select>
+                </div>
+                <div class="col-md-3">
+                  <div class="form-group">
+                    <label class="bmd-label-floating" name="email">Contacto</label>
+                    <input type="text" class="form-control input-border-width">
                   </div>
                 </div>
-
-                
+                <div class="col-md-3">
+                  <div class="form-group">
+                    <label class="bmd-label-floating">Email</label>
+                    <input type="email" class="form-control input-border-width" name="email">
+                  </div>
+                </div>
               </div>
               <div class="row">
                 <div class="col-md-12">
@@ -169,21 +151,21 @@
             </div>
             <div class="card-body">
 
-              {{-- <div class="form-group">
+              <div class="form-group">
                   <label class="bmd-label-floating">Genero</label>
-                  <input class="form-control input-border-width" type="text" list="lista-generos" name="id_gender" id="lista-generos">
+                  <input class="form-control input-border-width" type="text" list="lista-generos" name="id_gender">
                   <datalist id="lista-generos">
                     @foreach($genders as $gender)
-                      <option value="{{ $gender->name }}">
-                          {{ $gender->id }}
+                      <option value="{{ $gender->id }}">
+                          {{ $gender->name }}
                       </option>
                     @endforeach
                   </datalist>
-              </div> --}}
+              </div>
 
 
 
-              <div class="form-group">
+              {{-- <div class="form-group">
                 <label class="bmd-label-floating label-create-form">Genero</label>
                 <select name="id_gender" class="custom-select input-border-width">
                     @foreach($genders as $gender)
@@ -192,7 +174,7 @@
                           </option>
                       @endforeach
                 </select>
-              </div>
+              </div> --}}
 
 
 
@@ -234,19 +216,6 @@
               </div>
             </div>
             <div class="card-body">
-
-              <div class="form-group">
-                <label class="bmd-label-floating">Turma de Recrutamento</label>
-                <datalist id="turma">
-                  <option value="TPSI11.18">
-                  <option value="TPSI10.18">
-                  <option value="GR09.17">
-                  <option value="GR09.18">
-                  <option value="GR09.19">
-                </datalist>
-                <input list="turma" class="form-control input-border-width">
-              </div>
-
               <div class="form-group">
                 <label class="bmd-label-floating">Curso 1ª Opção</label>
                 <datalist id="turma">
@@ -312,8 +281,69 @@
                     <input list="origem" class="form-control input-border-width">
                   </div>
                 </div>
+              </div>
+
             </div>
+          </div>
         </div>
-    </div>
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-header card-header-text card-header-info">
+              <div class="card-text">
+                <h4 class="card-title">Documentos Entregues</h4>
+              </div>
+            </div>
+            <div class="card-body">
+
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" type="checkbox" value="">
+                  BI/CC
+                  <span class="form-check-sign">
+                    <span class="check"></span>
+                  </span>
+                </label><br><br>
+                <label class="form-check-label">
+                  <input class="form-check-input" type="checkbox" value="">
+                  Certificado de Habilitações
+                  <span class="form-check-sign">
+                    <span class="check"></span>
+                  </span>
+                </label><br><br>
+                <label class="form-check-label">
+                  <input class="form-check-input" type="checkbox" value="">
+                  Cartão de Utente ou Declaração Centro de Emprego
+                  <span class="form-check-sign">
+                    <span class="check"></span>
+                  </span>
+                </label><br><br>
+                <label class="form-check-label">
+                  <input class="form-check-input" type="checkbox" value="">
+                  Curriculum Vitae
+                  <span class="form-check-sign">
+                    <span class="check"></span>
+                  </span>
+                </label><br><br>
+                <label class="form-check-label">
+                  <input class="form-check-input" type="checkbox" value="">
+                  Registo Criminal
+                  <span class="form-check-sign">
+                    <span class="check"></span>
+                  </span>
+                </label><br><br>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-info pull-right">Criar</button>
+    </form>
+  </div>
 </div>
+<script>
+
+</script>
+
+
 @endsection
