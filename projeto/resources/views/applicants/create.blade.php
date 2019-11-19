32da9c17f159807965178e86dfb5460aa85fa76f
inesespinha <inesespinhaa@gmail.com>
2019-11-19 17:30:50 +0000

    candidatos

--- projeto/resources/views/applicants/create.blade.php
+++ projeto/resources/views/applicants/create.blade.php
@@ -28,112 +28,96 @@
                 </div>
                 <div class="col-md-3">
                     <label>Nome Completo</label>
-                    <input type="text" class="form-control" name="name">
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="name">
                 </div>
                 <div class="col-md-4">
-                  <label class="bg-white">Data-de-Nascimento</label>
-                  <input type="date" class="form-control" name="birthdate" format="dd/MM/yyyy">
+                  <label>Data-de-Nascimento</label>
+                  <input type="date" class="form-control border-top-0 border-left-0 border-right-0" name="birthdate" format="dd/MM/yyyy">
                 </div>
               </div>
-            </div>
 
               <div class="row">
                 <div class="col-md-4">
-                    <label class="bmd-label-floating">N. Contribuinte</label>
-                    <input type="text" class="form-control input-border-width" name="nif">
+                    <label>N. Contribuinte</label>
+                    <input type="text" class="form-control" name="nif">
                 </div>
                 <div class="col-md-4">
-                    <label class="bmd-label-floating">N. CC</label>
-                    <input type="text" class="form-control input-border-width" name="identityCard">
+                    <label>N. CC</label>
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="identityCard">
                 </div>
                 <div class="col-md-4">
-                    <label class="bmd-label-floating bg-white p-1">Data de Validade</label>
-                    <input type="date" class="form-control input-border-width" name="ccExpirationDate">
+                    <label>Data de Validade</label>
+                    <input type="date" class="form-control border-top-0 border-left-0 border-right-0" name="ccExpirationDate">
                 </div>
               </div>
+
               <div class="row">
                 <div class="col-md-4">
-                    <label class="bmd-label-floating">Naturalidade</label>
-                    <input type="text" class="form-control input-border-width" name="birthtown">
+                    <label>Naturalidade</label>
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="birthtown">
                 </div>
                 <div class="col-md-4">
-                    <label class="bmd-label-floating">Nacionalidade</label>
-                    <input type="text" class="form-control input-border-width" name="nationality">
+                    <label>Nacionalidade</label>
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="nationality">
                 </div>
                 <div class="col-md-4">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Estado Civil</label>
-                    <input type="text" class="form-control input-border-width" name="civilState">
-                  </div>
+                    <label>Estado Civil</label>
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="civilState">
                 </div>
               </div>
+
               <div class="row">
                 <div class="col-md-5">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Morada</label>
-                    <input type="text" class="form-control input-border-width" name="address">
-                  </div>
+                    <label>Morada</label>
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="address">
                 </div>
                 <div class="col-md-3">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Código Postal</label>
-                    <input type="text" class="form-control input-border-width">
-                  </div>
+                    <label>Código Postal</label>
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0">
                 </div>
                 <div class="col-md-4">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Localidade</label>
-                    <input type="text" class="form-control input-border-width" name="town">
-                  </div>
+                    <label>Localidade</label>
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="town">
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-3">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Concelho</label>
-                    <input type="text" class="form-control input-border-width" name="parish">
-                  </div>
+                    <label>Concelho</label>
+                    <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="parish">
                 </div>
                 <div class="col-md-3">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Distrito</label>
-                    <input type="text" class="form-control input-border-width" list="lista-distritos" name="district_id">
-                    <datalist id="lista-distritos">
-                      @foreach($districts as $district)
-                        <option value="{{ $district->id }}">
-                            {{ $district->name }}
-                        </option>
-                      @endforeach
-                    </datalist>
-                  </div>
+                  <label>Distrito</label>
+                  <input type="text" class="form-control border-top-0 border-left-0 border-right-0" list="lista-distritos" name="district_id">
+                  <datalist id="lista-distritos">
+                    @foreach($districts as $district)
+                      <option value="{{ $district->id }}">
+                          {{ $district->name }}
+                      </option>
+                    @endforeach
+                  </datalist>
                 </div>
                 <div class="col-md-3">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Contacto</label>
-                    <input type="text" class="form-control input-border-width" name="phoneNumber">
-                  </div>
+                  <label>Contacto</label>
+                  <input type="text" class="form-control border-top-0 border-left-0 border-right-0" name="phoneNumber">
                 </div>
                 <div class="col-md-3">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Email</label>
-                    <input type="email" class="form-control input-border-width" name="email">
-                  </div>
+                  <label>Email</label>
+                  <input type="email" class="form-control border-top-0 border-left-0 border-right-0" name="email">
                 </div>
               </div>
+
               <div class="row">
                 <div class="col-md-12">
-                  <div class="form-group">
-                    <label>Observações</label>
-                    <div class="form-group">
-                      <textarea class="form-control" rows="5" name="observations"></textarea>
-                    </div>
-                  </div>
+                  <label>Observações</label>
+                    <textarea class="form-control" rows="5" name="observations"></textarea>
                 </div>
               </div>
               <a class="float-right text-info" href="#">Preenchimento automático</a>
             </div>
           </div>
         </div>
+      </div>
+
         <div class="col-md-3">
           <div class="card">
             <div class="card-header card-header-text card-header-info">
@@ -143,21 +127,19 @@
             </div>
             <div class="card-body">
 
-              <div class="form-group">
-                  <label class="bmd-label-floating">Género</label>
-                  <input class="form-control input-border-width" type="text" list="lista-generos" name="gender_id">
-                  <datalist id="lista-generos" >
-                    @foreach($genders as $gender)
-                      <option value="{{ $gender->id }}">
-                          {{ $gender->name }}
-                      </option>
-                    @endforeach
-                  </datalist>
-              </div>
+              <label>Género</label>
+              <input class="form-control border-top-0 border-left-0 border-right-0" type="text" list="lista-generos" name="gender_id">
+              <datalist id="lista-generos" >
+                @foreach($genders as $gender)
+                  <option value="{{ $gender->id }}">
+                      {{ $gender->name }}
+                  </option>
+                @endforeach
+              </datalist>
 
               {{-- <div class="form-group">
                 <label class="bmd-label-floating label-create-form">Genero</label>
-                <select name="gender_id" class="custom-select input-border-width">
+                <select name="gender_id" class="custom-select border-top-0 border-left-0 border-right-0">
                     @foreach($genders as $gender)
                           <option value="{{ $gender->id }}">
                               {{ $gender->name }}
@@ -166,49 +148,41 @@
                 </select>
               </div> --}}
 
-              <div class="form-group">
-                <label class="bmd-label-floating">Estado Civil</label>
-                <datalist id="civil">
-                  <option value="Solteiro/a">
-                  <option value="Casado/a">
-                  <option value="Viuvo/a">
-                  <option value="Outro">
-                </datalist>
-                <input list="civil" class="form-control input-border-width">
-              </div>
-              <div class="form-group">
-                <label class="bmd-label-floating">Situação Face ao Emprego</label>
-                <input type="text" class="form-control input-border-width" list="lista-situacoes" name="unemployement_situation_id">
-                <datalist id="lista-situacoes">
-                  @foreach($unemployementSituations as $unemployementSituation)
-                    <option value="{{ $unemployementSituation->id }}">
-                        {{ $unemployementSituation->name }}
-                    </option>
-                  @endforeach
-                </datalist>
-              </div>
-              <div class="form-group">
-                <label class="bmd-label-floating">Habilitação Literárias</label>
-                <input type="text" class="form-control input-border-width" list="list-habilitacoes" name="education_id">
-                <datalist id="lista-situacoes">
-                  @foreach($educations as $education)
-                    <option value="{{ $education->id }}">
-                        {{ $education->name }}
-                    </option>
-                  @endforeach
-                </datalist>
-              </div>
-              <div class="form-group">
-                <label class="bmd-label-floating">Escola de proveniência</label>
-                <input type="text" class="form-control input-border-width" list="lista-escolas" name="provenance_school_id">
-                <datalist id="lista-escolas">
-                  @foreach($provenance_schools as $provenance_school)
-                    <option value="{{ $provenance_school->id }}">
-                        {{ $provenance_school->name }}
-                    </option>
-                  @endforeach
-                </datalist>
-              </div>
+              <label>Estado Civil</label>
+              <datalist id="civil">
+                <option value="Solteiro/a">
+                <option value="Casado/a">
+                <option value="Viuvo/a">
+                <option value="Outro">
+              </datalist>
+              <input list="civil" class="form-control border-top-0 border-left-0 border-right-0">
+              <label>Situação Face ao Emprego</label>
+              <input type="text" class="form-control border-top-0 border-left-0 border-right-0" list="lista-situacoes" name="unemployement_situation_id">
+              <datalist id="lista-situacoes">
+                @foreach($unemployementSituations as $unemployementSituation)
+                  <option value="{{ $unemployementSituation->id }}">
+                      {{ $unemployementSituation->name }}
+                  </option>
+                @endforeach
+              </datalist>
+              <label>Habilitação Literárias</label>
+              <input type="text" class="form-control border-top-0 border-left-0 border-right-0" list="list-habilitacoes" name="education_id">
+              <datalist id="lista-situacoes">
+                @foreach($educations as $education)
+                  <option value="{{ $education->id }}">
+                      {{ $education->name }}
+                  </option>
+                @endforeach
+              </datalist>
+              <label>Escola de proveniência</label>
+              <input type="text" class="form-control border-top-0 border-left-0 border-right-0" list="lista-escolas" name="provenance_school_id">
+              <datalist id="lista-escolas">
+                @foreach($provenance_schools as $provenance_school)
+                  <option value="{{ $provenance_school->id }}">
+                      {{ $provenance_school->name }}
+                  </option>
+                @endforeach
+              </datalist>
             </div>
           </div>
         </div>
@@ -222,69 +196,57 @@
               </div>
             </div>
             <div class="card-body">
-              <div class="form-group">
-                <label class="bmd-label-floating">Curso 1ª Opção</label>
-                <input list="turma" class="form-control input-border-width" list="lista-cursos1" name="first_option_course_id">
-                <datalist id="lista-cursos1">
-                  @foreach($courses as $course)
-                    <option value="{{ $course->id }}">
-                        {{ $course->name }}
-                    </option>
-                  @endforeach
-                </datalist>
-              </div>
-              <div class="form-group">
-                <label class="bmd-label-floating">Curso 2ª Opção</label>
-                <input list="turma" class="form-control input-border-width" list="lista-cursos2" name="second_option_course_id">
-                <datalist id="lista-cursos2">
-                  @foreach($courses as $course)
-                    <option value="{{ $course->id }}">
-                        {{ $course->name }}
-                    </option>
-                  @endforeach
-                </datalist>
-              </div>
+              <label>Curso 1ª Opção</label>
+              <input list="turma" class="form-control border-top-0 border-left-0 border-right-0" list="lista-cursos1" name="first_option_course_id">
+              <datalist id="lista-cursos1">
+                @foreach($courses as $course)
+                  <option value="{{ $course->id }}">
+                      {{ $course->name }}
+                  </option>
+                @endforeach
+              </datalist>
+              <label>Curso 2ª Opção</label>
+              <input list="turma" class="form-control border-top-0 border-left-0 border-right-0" list="lista-cursos2" name="second_option_course_id">
+              <datalist id="lista-cursos2">
+                @foreach($courses as $course)
+                  <option value="{{ $course->id }}">
+                      {{ $course->name }}
+                  </option>
+                @endforeach
+              </datalist>
               <div class="row">
                 <div class="col-md-6">
-                  <div class="form-group">
-                    <label class="bmd-label-floating bg-white p-1">Data de Candidatura</label>
-                    <input type="date" class="form-control input-border-width" name="applicationDate">
-                  </div>
+                  <label>Data de Candidatura</label>
+                  <input type="date" class="form-control border-top-0 border-left-0 border-right-0" name="applicationDate">
                 </div>
                 <div class="col-md-6">
-                  <div class="form-group">
-                    <label class="bmd-label-floating bg-white p-1">Data de Anulação</label>
-                    <input type="date" class="form-control input-border-width">
-                  </div>
+                  <label>Data de Anulação</label>
+                  <input type="date" class="form-control border-top-0 border-left-0 border-right-0">
                 </div>
               </div>
 
               <div class="row">
                 <div class="col-md-6">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Origem</label>
-                    <input list="lista-origens" class="form-control input-border-width" name="origin_id">
-                    <datalist id="lista-origens">
-                      @foreach($origins as $origin)
-                        <option value="{{ $origin->id }}">
-                            {{ $origin->name }}
+                  <label>Origem</label>
+                  <input list="lista-origens" class="form-control border-top-0 border-left-0 border-right-0" name="origin_id">
+                  <datalist id="lista-origens">
+                    @foreach($origins as $origin)
+                      <option value="{{ $origin->id }}">
+                          {{ $origin->name }}
+                      </option>
+                    @endforeach
+                  </datalist>
+                </div>
+                <div class="col-md-6">
+                  <label>Motivo da Anulação</label>
+                  <input class="form-control border-top-0 border-left-0 border-right-0" list="lista-motivos" name="cancellation_reason_id">
+                  <datalist id="lista-motivos">
+                      @foreach($cancellationReasons as $cancellationReason)
+                        <option value="{{ $cancellationReason->id }}">
+                            {{ $cancellationReason->name }}
                         </option>
                       @endforeach
                     </datalist>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <label class="bmd-label-floating">Motivo da Anulação</label>
-                    <input class="form-control input-border-width" list="lista-motivos" name="cancellation_reason_id">
-                    <datalist id="lista-motivos">
-                        @foreach($cancellationReasons as $cancellationReason)
-                          <option value="{{ $cancellationReason->id }}">
-                              {{ $cancellationReason->name }}
-                          </option>
-                        @endforeach
-                      </datalist>
-                  </div>
                 </div>
               </div>
             </div>
