ddd72c9dd288a757214ea65c2600888a9769ff0c
sergiomoura <sergio.moura.t0114628@edu.atec.pt>
2019-11-22 02:00:19 +0000

    finished i think

--- projeto/resources/views/rsclasses/index.blade.php
+++ projeto/resources/views/rsclasses/index.blade.php
@@ -7,6 +7,11 @@
         </button>
     </a>
 </div>
+<div class="col-md-12 text-center">
+    <button type="button" class="btn btn-info" onclick="deleteSelected()">
+        Eliminar Turma(as)
+    </button>
+</div>
 
 <div class="content">
     <div class="container-fluid">
@@ -34,7 +39,8 @@
                                         <tr>
                                             <td>
                                                 <div class="form-check">
-                                                    <label class="form-check-label">                                                        
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input usersSelect" type="checkbox" value="{{$rsClass->id}}">
                                                         <span class="form-check-sign">
                                                         <span class="check"></span>
                                                         </span>
@@ -66,13 +72,9 @@
                                                         <i class="material-icons">edit</i>
                                                     </button>
                                                 </a>                                            
-                                            <form action="/rsclasses/{{ $rsClass->id }}" method="post">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button type="submit" rel="tooltip" title="Remover" class="btn btn-info btn-link btn-sm border-0" value="DELETE">
-                                                        <i class="material-icons">close</i>
-                                                    </button>
-                                                </form>
+                                                <button type="submit" value="{{ $rsClass->id }}" onclick="openModal(this.value)" rel="tooltip" title="Remover" class="btn btn-info btn-link btn-sm border-0" value="DELETE">
+                                                    <i class="material-icons">close</i>
+                                                </button>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -86,4 +88,66 @@
     </div>
 </div>
 
+
+<button id="btnModalShow" type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalEventsShow" style="visibility: hidden;"></button>
+
+<div class="modal fade" id="modalEventsShow" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+  <input type="hidden" id="idEvent"/>
+    <div id="modalEvents" class="modal-content">
+        <div class="modal-header">
+            <p class="modal-title">
+                Tem a certeza que pretende eliminar?
+            </p>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <p>Este registo será totalmente perdido!
+
+            </p>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                Não
+            </button>
+            <form id="formModal" action="" method="post">
+                @csrf
+                @method('delete')
+                <button type="submit" class="btn btn-danger" value="DELETE" style="margin-top: 24px;">
+                    Sim, eliminar
+                </button>
+            </form>
+        </div>
+    </div>
+  </div>
+</div>
+<script>
+    function openModal(data)
+    {
+        document.getElementById('formModal').action = "/rsclasses/" + data;
+        $('#btnModalShow').click();
+    }
+
+    function deleteSelected()
+    {
+        let allSelected = document.getElementsByClassName('usersSelect');
+        let count = 0;
+        for(let i = 0; i < allSelected.length; i++){
+            if (allSelected[i].checked){
+                $.ajax({
+                    contentType: "application/json",
+                    url:"/api/rsclasses/" + allSelected[i].value,
+                    type:"DELETE",
+                    data:{},
+                    success:function(data)
+                    { 
+                    }
+                });
+            }
+        }
+        location.reload();
+    }
+</script>
 @endsection
